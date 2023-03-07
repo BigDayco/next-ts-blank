c1c68adf413916d6a4eddcde6473ef9f51df96fb
Henrique de Castro <hendecastro@gmail.com>
2023-03-07 13:01:24 -0300

    chore: update dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
+import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
-
-import { Inter } from '@next/font/google'
 
 import styles from '~/styles/Home.module.css'
 
